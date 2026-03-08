d1b7d01352039f0126625b31aba9824f9663ebf6
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-03-08 16:26:13 +0530

    minor update

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -1,48 +1,48 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactNative from "eslint-plugin-react-native";
-import importPlugin from "eslint-plugin-import";
-import prettier from "eslint-plugin-prettier";
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactNative from 'eslint-plugin-react-native'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
 
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
 
     languageOptions: {
       parser: tseslint.parser,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
 
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
         ecmaFeatures: { jsx: true },
       },
 
       globals: {
-        __DEV__: "readonly",
-        console: "readonly",
-        process: "readonly",
-        React: "readonly",
+        __DEV__: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
+        React: 'readonly',
       },
     },
 
     plugins: {
-      "@typescript-eslint": tseslint.plugin,
+      '@typescript-eslint': tseslint.plugin,
       react,
-      "react-hooks": reactHooks,
-      "react-native": reactNative,
+      'react-hooks': reactHooks,
+      'react-native': reactNative,
       import: importPlugin,
       prettier,
     },
 
     settings: {
-      react: { version: "detect" },
-      "import/resolver": {
+      react: { version: 'detect' },
+      'import/resolver': {
         typescript: true,
       },
     },
@@ -54,12 +54,12 @@ export default [
       =====================================
       */
 
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "react/jsx-key": "error",
-      "react/jsx-no-duplicate-props": "error",
-      "react/jsx-no-undef": "error",
-      "react/no-direct-mutation-state": "error",
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-key': 'error',
+      'react/jsx-no-duplicate-props': 'error',
+      'react/jsx-no-undef': 'error',
+      'react/no-direct-mutation-state': 'error',
 
       /*
       =====================================
@@ -67,8 +67,8 @@ export default [
       =====================================
       */
 
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
 
       /*
       =====================================
@@ -76,13 +76,13 @@ export default [
       =====================================
       */
 
-      "react-native/no-unused-styles": "error",
+      'react-native/no-unused-styles': 'error',
 
       // NativeWind requires inline styles
-      "react-native/no-inline-styles": "off",
+      'react-native/no-inline-styles': 'off',
 
-      "react-native/no-raw-text": "off",
-      "react-native/no-color-literals": "off",
+      'react-native/no-raw-text': 'off',
+      'react-native/no-color-literals': 'off',
 
       /*
       =====================================
@@ -90,35 +90,29 @@ export default [
       =====================================
       */
 
-      "no-unused-vars": "off",
+      'no-unused-vars': 'off',
 
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
         },
       ],
 
-      "@typescript-eslint/no-explicit-any": "warn",
+      '@typescript-eslint/no-explicit-any': 'warn',
 
-      "@typescript-eslint/consistent-type-imports": [
-        "error",
-        { prefer: "type-imports" },
-      ],
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
 
-      "@typescript-eslint/no-shadow": "error",
+      '@typescript-eslint/no-shadow': 'error',
 
-      "@typescript-eslint/no-empty-function": [
-        "warn",
-        { allow: ["arrowFunctions"] },
-      ],
+      '@typescript-eslint/no-empty-function': ['warn', { allow: ['arrowFunctions'] }],
 
-      "@typescript-eslint/no-non-null-assertion": "warn",
+      '@typescript-eslint/no-non-null-assertion': 'warn',
 
-      "@typescript-eslint/prefer-optional-chain": "error",
+      '@typescript-eslint/prefer-optional-chain': 'error',
 
-      "@typescript-eslint/no-unnecessary-type-assertion": "error",
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
 
       /*
       =====================================
@@ -126,26 +120,26 @@ export default [
       =====================================
       */
 
-      "import/order": [
-        "error",
+      'import/order': [
+        'error',
         {
           groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-            "type",
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'type',
           ],
-          "newlines-between": "always",
-          alphabetize: { order: "asc", caseInsensitive: true },
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
         },
       ],
 
-      "import/no-duplicates": "error",
-      "import/first": "error",
-      "import/newline-after-import": "error",
+      'import/no-duplicates': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
 
       /*
       =====================================
@@ -153,19 +147,19 @@ export default [
       =====================================
       */
 
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      "no-debugger": "error",
-      "no-alert": "error",
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-alert': 'error',
 
-      "prefer-const": "error",
-      "no-var": "error",
+      'prefer-const': 'error',
+      'no-var': 'error',
 
-      "object-shorthand": "error",
-      "prefer-template": "error",
+      'object-shorthand': 'error',
+      'prefer-template': 'error',
 
-      eqeqeq: ["error", "always", { null: "ignore" }],
+      eqeqeq: ['error', 'always', { null: 'ignore' }],
 
-      "no-return-await": "error",
+      'no-return-await': 'error',
 
       /*
       =====================================
@@ -173,10 +167,10 @@ export default [
       =====================================
       */
 
-      "arrow-body-style": ["error", "as-needed"],
-      "prefer-arrow-callback": "error",
-      "no-useless-return": "error",
-      "no-else-return": "error",
+      'arrow-body-style': ['error', 'as-needed'],
+      'prefer-arrow-callback': 'error',
+      'no-useless-return': 'error',
+      'no-else-return': 'error',
 
       /*
       =====================================
@@ -184,16 +178,16 @@ export default [
       =====================================
       */
 
-      "prettier/prettier": [
-        "error",
+      'prettier/prettier': [
+        'error',
         {
           singleQuote: true,
           semi: false,
-          trailingComma: "all",
+          trailingComma: 'all',
           tabWidth: 2,
           printWidth: 90,
-          arrowParens: "always",
-          plugins: ["prettier-plugin-tailwindcss"],
+          arrowParens: 'always',
+          plugins: ['prettier-plugin-tailwindcss'],
         },
       ],
     },
@@ -206,16 +200,11 @@ export default [
   */
 
   {
-    files: [
-      "eslint.config.*",
-      "metro.config.*",
-      "babel.config.*",
-      "tailwind.config.*",
-    ],
+    files: ['eslint.config.*', 'metro.config.*', 'babel.config.*', 'tailwind.config.*'],
 
     rules: {
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/no-explicit-any": "off",
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 
@@ -227,14 +216,14 @@ export default [
 
   {
     ignores: [
-      "node_modules",
-      ".expo",
-      "dist",
-      "build",
-      "coverage",
-      "ios",
-      "android",
-      "web-build",
+      'node_modules',
+      '.expo',
+      'dist',
+      'build',
+      'coverage',
+      'ios',
+      'android',
+      'web-build',
     ],
   },
-];
+]
